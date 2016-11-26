b6a93fda876e43d1e52f3411622f1d64c7fe4332
Seth <you@example.com>
2016-11-25 21:06:16 -0500

    Tests 2/2 Green

--- src/dolman/vendingmachinekata/Cashbox.java
+++ src/dolman/vendingmachinekata/Cashbox.java
@@ -2,12 +2,21 @@ package dolman.vendingmachinekata;
 
 public class Cashbox {
 
+	Integer cashInserted;
+	
 	public void insertCoin(String coin) {
-		
+		switch(coin) {
+		case "Nickel":
+			cashInserted = 5;
+			break;
+		case "Dime":
+			cashInserted = 10;
+			break;
+		}
 	}
 
 	public String getCashInserted() {
-		return "5";
+		return cashInserted.toString();
 	}
 
 }
